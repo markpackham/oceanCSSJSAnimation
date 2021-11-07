71dc65be3dd5aa42087d44f2a413205fffcaa02d
mark <markpackham1@gmail.com>
2021-11-07 16:09:02 +0000

    chose trash icons by fund raising amount
    https://youtu.be/AWCCgQMwdik?t=1410

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const currencyFormatter = new Intl.NumberFormat("en-us", {
   currency: "USD",
   maximumFractionDigits: 0,
 });
+// Make sure always 8 digits long (can have 0s at the front of it if needed, eg 00123456)
 const trashFormatter = new Intl.NumberFormat("en-us", {
   minimumIntegerDigits: 8,
   maximumFractionDigits: 0,
@@ -21,4 +22,37 @@ async function setupTrash() {
     .then((res) => res.json())
     .then((data) => data.count);
   moneyElem.innerText = currencyFormatter.format(amountRaised);
+
+  // don't want to end up with negative numbers so either positive or 0
+  const amountLeftToRaise = Math.max(MAX_MONEY_RAISED - amountRaised, 0);
+  const stringifiedAmount = trashFormatter.format(amountLeftToRaise);
+  // differnt items shown depending on the amount left to raise,
+  // large items on left end of number small items on right
+  // if amount "12345678" then xxl is "12" and we render 12 bags and xl is "3"
+  const trashAmount = {
+    xxl: {
+      amount: parseInt(`${stringifiedAmount[0]}${stringifiedAmount[1]}`),
+      icon: "bag",
+    },
+    xl: {
+      amount: parseInt(stringifiedAmount[2]),
+      icon: "takeout",
+    },
+    lg: {
+      amount: parseInt(stringifiedAmount[3]),
+      icon: "headphones",
+    },
+    md: {
+      amount: parseInt(stringifiedAmount[4]),
+      icon: "phone",
+    },
+    sm: {
+      amount: parseInt(stringifiedAmount[5]),
+      icon: "toy-car",
+    },
+    xs: {
+      amount: parseInt(stringifiedAmount[6]),
+      icon: "bottle",
+    },
+  };
 }
